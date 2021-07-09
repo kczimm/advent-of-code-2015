a13c80ca0e540473294266a38fb9aa609120d75e
Kevin C. Zimmerman <kevinczimmerman@gmail.com>
2021-07-09 16:53:14 -0500

    day 1 part 1 and 2

---
+++ src/day6/main.rs
@@ -0,0 +1,19 @@
+
+use util::load;
+
+use std::io;
+
+fn main() -> io::Result<()> {
+    let _contents = load("src/day6/input.txt")?;
+
+    Ok(())
+}
+
+# [cfg(test)]
+mod tests {
+
+    # [test]
+    fn test() {
+
+    }
+}
---
+++ src/day7/main.rs
@@ -0,0 +1,19 @@
+
+use util::load;
+
+use std::io;
+
+fn main() -> io::Result<()> {
+    let _contents = load("src/day7/input.txt")?;
+
+    Ok(())
+}
+
+# [cfg(test)]
+mod tests {
+
+    # [test]
+    fn test() {
+
+    }
+}
