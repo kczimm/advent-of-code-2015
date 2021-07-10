505e2d9e6a54cfb3480cc77d93117045bcbaff29
Kevin C. Zimmerman <kevinczimmerman@gmail.com>
2021-07-09 22:04:56 -0500

    day 4 part 2

--- src/day4/main.rs
+++ src/day4/main.rs
@@ -9,6 +9,11 @@ For example:
 
     If your secret key is abcdef, the answer is 609043, because the MD5 hash of abcdef609043 starts with five zeroes (000001dbbfa...), and it is the lowest such number to do so.
     If your secret key is pqrstuv, the lowest number it combines with to make an MD5 hash starting with five zeroes is 1048970; that is, the MD5 hash of pqrstuv1048970 looks like 000006136ef....
+
+--- Part Two ---
+
+Now find one that starts with six zeroes.
+
 */
 use md5;
 use std::io;
@@ -16,15 +21,16 @@ use std::io;
 fn main() -> io::Result<()> {
     let secret: SecretKey = "iwrupvqb";
 
-    println!("part1: {}", lowest_number_with_five_leading_zeros(secret));
+    println!("part1: {}", lowest_number_with_n_leading_zeros(secret, 5));
+    println!("part2: {}", lowest_number_with_n_leading_zeros(secret, 6));
 
     Ok(())
 }
 
-fn lowest_number_with_five_leading_zeros(secret: SecretKey) -> Answer {
+fn lowest_number_with_n_leading_zeros(secret: SecretKey, n: usize) -> Answer {
     let mut answer = 0;
 
-    while !has_five_leading_zeros(&make_hash(secret, answer)) {
+    while !has_n_leading_zeros(&make_hash(secret, answer), n) {
         answer += 1;
     }
 
@@ -38,13 +44,13 @@ fn make_hash(secret: SecretKey, answer: Answer) -> String {
     format!("{:x}", md5::compute(format!("{}{}", secret, answer)))
 }
 
-fn has_five_leading_zeros(s: &str) -> bool {
-    if s.len() < 5 {
+fn has_n_leading_zeros(s: &str, n: usize) -> bool {
+    if s.len() < n {
         false
     } else {
         s.chars()
             .enumerate()
-            .filter(|(i, _)| *i < 5)
+            .filter(|(i, _)| *i < n)
             .all(|(_, c)| c == '0')
     }
 }
@@ -57,19 +63,19 @@ mod tests {
     fn test_has_five_leading_zeros() {
         let secret: SecretKey = "abcdef";
         let answer = 609043;
-        assert!(has_five_leading_zeros(&make_hash(secret, answer)));
+        assert!(has_n_leading_zeros(&make_hash(secret, answer), 5));
 
         let secret: SecretKey = "pqrstuv";
         let answer = 1048970;
-        assert!(has_five_leading_zeros(&make_hash(secret, answer)));
+        assert!(has_n_leading_zeros(&make_hash(secret, answer), 5));
     }
 
     #[test]
     fn test_lowest_number_with_five_leading_zeros() {
         let secret: SecretKey = "abcdef";
-        assert_eq!(lowest_number_with_five_leading_zeros(secret), 609043);
+        assert_eq!(lowest_number_with_n_leading_zeros(secret, 5), 609043);
 
         let secret: SecretKey = "pqrstuv";
-        assert_eq!(lowest_number_with_five_leading_zeros(secret), 1048970);
+        assert_eq!(lowest_number_with_n_leading_zeros(secret, 5), 1048970);
     }
 }
